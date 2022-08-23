332e88dac2a40f8864844d070acd8746b68ab54c
Gyeop <yanggieop@gmail.com>
2022-08-23 20:12:55 +0900

    feat: 아이콘 기본 인터페이스 제작

---
+++ packages/icons/.svgrrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  expandProps: false,
+  dimensions: true,
+  jsxRuntime: "automatic",
+  filenameCase: "pascal",
+  outDir: "generated",
+  ext: "res",
+  template: require("./template/svgr-cli.template"),
+  svgProps: {
+    width: "{size}",
+    height: "{size}",
+    style: "{style}",
+    className: "{className}",
+    viewBox: "0 0 24 24",
+    fill: "none",
+  },
+  replaceAttrValues: {
+    "#000": "{fill}",
+  },
+};
---
+++ packages/icons/template/svgr-cli.template.js
@@ -0,0 +1,14 @@
+function template({ jsx }, { tpl }) {
+  const props = `(~size, ~style, ~className, ~fill)`;
+  const rescriptComponentDecorator = `@react.component`;
+  const rescriptComponentConverter = `let make = ${props} =>`;
+
+  const rescriptComponentInterface = `
+      ${rescriptComponentDecorator}
+      ${rescriptComponentConverter}
+    `;
+
+  return tpl`${rescriptComponentInterface} (${jsx})`;
+}
+
+module.exports = template;
