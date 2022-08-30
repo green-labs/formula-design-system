75dda2c98a48885b4ab593d9f91d094415d31a51
Minsang Kim <minsang.kim@greenlabs.co.kr>
2022-08-30 22:00:20 +0900

    feat: updated story example

--- packages/components/src/Button/Button.stories.js
+++ packages/components/src/Button/Button.stories.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { Button } from './Button.bs';
+import * as Button from './Button.bs';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
+export default { 
   title: 'Example/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-};
+} ;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => React.createElement(Button.make,args);
