a429dfea3b51eef9c37a41112e38e0112ac7b774
Jaeho Lee <me@jaeholee.org>
2022-09-13 21:36:32 +0900

    feat(design-token): Update tailwind token
    - add opacity, spacing, viewport, etc typo attrs
    - automate simple ref/sys tokens addition

--- packages/design-token/formats/tailwind.js
+++ packages/design-token/formats/tailwind.js
@@ -3,7 +3,7 @@ const prefix = ""
 /**
  * @param { import("style-dictionary/types/TransformedToken").TransformedToken[] arr }
  */
-const groupBy = (arr) =>
+const classify = (arr) =>
   arr.reduce((dict, el) => {
     const accessor = `${prefix}${el.attributes.item}`
     const subAccessor = el.attributes.subitem
@@ -21,16 +21,43 @@ const groupBy = (arr) =>
     return dict
   }, {})
 
+const classifyTypographyTokens = (arr) =>
+  arr.reduce((dict, el) => {
+    const key = el.path.slice(2, el.path.length).join("-")
+    dict[key] = el.value
+
+    return dict
+  }, {})
+
 /**
  * @param { import("style-dictionary/types/Format").FormatterArguments args }
  */
 module.exports = function (args) {
   const { dictionary } = args
-  // color tokens
-  const colorTokens = dictionary.allTokens.filter((e) => e.attributes.type === "color")
 
-  const ret = {
-    colors: groupBy(colorTokens),
+  // special tokens
+  const typography = classifyTypographyTokens(
+    dictionary.allTokens.filter((e) => e.attributes.type === "font" && e.attributes.category === "sys")
+  )
+  const tokens = {
+    typography,
   }
-  return JSON.stringify(ret)
+
+  const result = [
+    "opacity",
+    "color",
+    "spacing",
+    "viewport",
+    // etc typography tokens
+    "typeface",
+    "leading",
+    "weight",
+    "tracking",
+    "font-size",
+  ].reduce((ret, type) => {
+    ret[type] = classify(dictionary.allTokens.filter((e) => e.attributes.type === type))
+    return ret
+  }, tokens)
+
+  return JSON.stringify(result)
 }
