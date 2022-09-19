0d08ec4cf27e571790793cdff054ec6a084fabc7
Jaeho Lee <me@jaeholee.org>
2022-09-19 23:23:01 +0900

    feat(components): setup rollup build

---
+++ packages/components/rollup.config.mjs
@@ -0,0 +1,48 @@
+import { readFileSync } from "fs"
+import * as path from "path"
+
+import { vanillaExtractPlugin } from "@vanilla-extract/rollup-plugin"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import { babel } from "@rollup/plugin-babel"
+import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json"
+
+const pkg = JSON.parse(readFileSync(new URL("./package.json", import.meta.url).pathname))
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"]
+
+export default {
+  plugins: [
+    nodeResolve({ extensions }),
+    commonjs(),
+    babel({
+      extensions,
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+      envName: "production",
+      targets: "last 1 chrome versions",
+    }),
+    json({
+      compact: true,
+    }),
+    vanillaExtractPlugin(),
+  ],
+  input: "./src/index.ts",
+  output: [
+    {
+      format: "cjs",
+      dir: path.dirname(pkg.main),
+      preserveModules: true,
+      preserveModulesRoot: "src",
+      entryFileNames: "[name][assetExtname].js",
+      exports: "named",
+    },
+    {
+      format: "esm",
+      dir: path.dirname(pkg.module),
+      preserveModules: true,
+      preserveModulesRoot: "src",
+      entryFileNames: "[name][assetExtname].js",
+    },
+  ],
+}
