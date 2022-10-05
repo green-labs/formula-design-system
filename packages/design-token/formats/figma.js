41cbeb50d928d410c5cdeed8a4e703f854ab6e18
Jaeho Lee <me@jaeholee.org>
2022-10-05 13:11:06 +0700

    feat(design-token): change figma tokens output structure
    - GDS-114

--- packages/design-token/formats/figma.js
+++ packages/design-token/formats/figma.js
@@ -1,48 +1,97 @@
+// /**
+//  * @param { import("style-dictionary/types/TransformedToken").TransformedToken[] arr }
+//  */
+// const groupBy = (arr, type) =>
+//   arr.reduce((dict, el) => {
+//     const accessor = `${el.attributes.item}`
+//     const subAccessor = el.attributes.subitem
+//     const category = el.attributes.category
+
+//     // ref의 경우 Style을 하나의 카테고리로 몰아넣기 위해, 한단계 더 'ref' 필드를 만든다.
+//     // 이런 분기처리를 위해 최상위가 변경되는 'root' 개념이 들어간다.
+//     let root
+//     if (category === "ref") {
+//       if (!dict[category]) {
+//         dict[category] = {}
+//         dict[category][category] = {}
+//       }
+//       root = dict[category][category]
+//     } else {
+//       if (!dict[category]) {
+//         dict[category] = {}
+//       }
+//       root = dict[category]
+//     }
+
+//     if (!root[accessor]) {
+//       root[accessor] = {}
+//     }
+
+//     if (subAccessor) {
+//       root[accessor][subAccessor] = { value: el.value, type }
+//     } else {
+//       root[accessor] = { value: el.value, type }
+//     }
+
+//     return dict
+//   })
+
 /**
  * @param { import("style-dictionary/types/TransformedToken").TransformedToken[] arr }
  */
-const groupBy = (arr, type) =>
-  arr.reduce(
-    (dict, el) => {
-      const accessor = `${el.attributes.item}`
-      const subAccessor = el.attributes.subitem
-      const category = el.attributes.category
+const groupByColor = (arr) => {
+  const type = "color"
+  return arr.reduce((dict, el) => {
+    const accessor = `${el.attributes.item}`
+    const subAccessor = el.attributes.subitem
+    const category = el.attributes.category
 
-      // ref의 경우 Style을 하나의 카테고리로 몰아넣기 위해, 한단계 더 'ref' 필드를 만든다.
-      // 이런 분기처리를 위해 최상위가 변경되는 'root' 개념이 들어간다.
-      let root
-      if (category === "ref") {
-        if (!dict[category]) {
-          dict[category] = {}
-          dict[category][category] = {}
-        }
-        root = dict[category][category]
-      } else {
-        if (!dict[category]) {
-          dict[category] = {}
-        }
-        root = dict[category]
+    // ref의 경우 Style을 하나의 카테고리로 몰아넣기 위해, 한단계 더 'ref' 필드를 만든다.
+    // 이런 분기처리를 위해 최상위가 변경되는 'root' 개념이 들어간다.
+    let root
+    if (category === "ref") {
+      if (!dict[category]) {
+        dict[category] = {}
+        dict[category][category] = {}
       }
-
-      if (!root[accessor]) {
-        root[accessor] = {}
+      root = dict[category][category] // .ref.ref
+    } else if (category === "sys") {
+      if (!dict[category]) {
+        dict[category] = {}
       }
-
-      if (subAccessor) {
-        root[accessor][subAccessor] = { value: el.value, type }
-      } else {
-        root[accessor] = { value: el.value, type }
-      }
-
-      return dict
-    },
-    {
-      $themes: [],
-      $metadata: {
-        tokenSetOrder: ["sys", "ref"],
-      },
+      root = dict[category] // .sys
     }
-  )
+
+    if (category === "sys") {
+      const path =
+        (subAccessor ?? "container") === "container" ? "container" : "contents" // divider, outline, background,
+
+      if (!root[path]) {
+        root[path] = {}
+      }
+
+      let subPath = accessor
+      if (subAccessor === "container-contents") {
+        subPath = `on-${accessor}`
+      }
+
+      root[path][subPath] = { value: el.value, type }
+      return dict
+    }
+
+    if (!root[accessor]) {
+      root[accessor] = {}
+    }
+
+    if (subAccessor) {
+      root[accessor][subAccessor] = { value: el.value, type }
+    } else {
+      root[accessor] = { value: el.value, type }
+    }
+
+    return dict
+  }, {})
+}
 
 /**
  * @param { import("style-dictionary/types/Format").FormatterArguments args }
@@ -50,10 +99,16 @@ const groupBy = (arr, type) =>
 module.exports = function (args) {
   const { dictionary } = args
   // color tokens
-  const colorTokens = dictionary.allTokens.filter((e) => e.attributes.type === "color")
+  const colorTokens = dictionary.allTokens.filter(
+    (e) => e.attributes.type === "color"
+  )
 
   const ret = {
-    ...groupBy(colorTokens, "color"),
+    $themes: [],
+    $metadata: {
+      tokenSetOrder: ["sys", "ref"],
+    },
+    ...groupByColor(colorTokens),
   }
   return JSON.stringify(ret)
 }
