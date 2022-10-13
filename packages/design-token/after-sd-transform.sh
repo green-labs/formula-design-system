cdd200a96f390a95ad0c76a39044ba6a34ee506f
Jaeho Lee <me@jaeholee.org>
2022-10-13 09:26:03 +0800

    chore(design-token): use prettier to format SD output

--- packages/design-token/after-sd-transform.sh
+++ packages/design-token/after-sd-transform.sh
@@ -1,1 +1,3 @@
 cp dist/Formula__ColorMap.res ../components-rescript/src/generated/Formula__ColorMap.res
+cd "../../" # FIXME: how to use root path utils
+yarn prettier --write --ignore-unknown packages/design-token/dist/**/*.{js,ts,jsx,tsx,json,json5}
