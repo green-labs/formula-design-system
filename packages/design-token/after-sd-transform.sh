86c292fc51da9c48c3baad92befe5c6c9cd99198
Jaeho Lee <me@jaeholee.org>
2022-10-13 09:36:12 +0800

    chore: apply prettier option
    - --no-error-on-unmatched-pattern

--- packages/design-token/after-sd-transform.sh
+++ packages/design-token/after-sd-transform.sh
@@ -1,3 +1,4 @@
 cp dist/Formula__ColorMap.res ../components-rescript/src/generated/Formula__ColorMap.res
 cd "../../" # FIXME: how to use root path utils
-yarn prettier --write --ignore-unknown packages/design-token/dist/**/*.{js,ts,jsx,tsx,json,json5}
+echo "\nformatting files...\n"
+yarn prettier --write --ignore-unknown --no-error-on-unmatched-pattern packages/design-token/dist/**/*.{js,ts,jsx,tsx,json,json5}
