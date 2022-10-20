7c898e473220193b62b5269eebe0897b21032242
Minsang Kim <minsang.kim@greenlabs.co.kr>
2022-10-20 11:07:15 +0900

    fix: variantChecker 의 key constraint 추가

--- packages/components/src/util.ts
+++ packages/components/src/util.ts
@@ -1,5 +1,7 @@
-// @ts-expect-error
-export function variantChecker<K, V>(variant: K, variantMap: Record<K, V>) {
+export function variantChecker<K extends string, V>(
+  variant: K,
+  variantMap: Record<K, V>
+) {
   if (process.env.NODE_ENV !== "production" && !(variant in variantMap)) {
     console.error(`You have used non-exist variant key ${variant}.`)
   }
