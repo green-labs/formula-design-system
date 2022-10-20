238a723ad66a73da2c269436632dffc081e06727
Gyeop <yanggieop@gmail.com>
2022-10-20 17:17:19 +0900

    fix: 스테이트 레이어 계산 함수 분리

--- packages/components/src/Button/utils.ts
+++ packages/components/src/Button/utils.ts
@@ -1,14 +1,9 @@
-import chroma from "chroma-js"
-import { overlay } from "color-blend"
-import { colorMap, tokens } from "@greenlabs/formula-design-token"
 import {
   buttonVariantColors,
   dynamicStyles,
 } from "./ContainerButton/styles.css"
 import type { buttonSize, buttonVariants } from "./buttonCommonTypes"
-
-const { color } = tokens.sys
-const OPACITY_VALUES = { HOVER: 0.04, ACTIVE: 0.08 } as const
+import { getBlendLayerColor, getLayeredColor } from "../stateLayers"
 
 export const getNotificationCountBadgeSize = (size: buttonSize) => {
   switch (size) {
@@ -37,50 +32,6 @@ export const getIconSize = (size: buttonSize) => {
   }
 }
 
-type RGBA = { r: number; g: number; b: number; a: number }
-const get_RGBA_Object = ([r, g, b, a]: number[]): RGBA => ({ r, g, b, a })
-const get_RGBA_CSS = ({ r, g, b, a }: RGBA) => `rgba(${r},${g},${b},${a})`
-
-// opacity - number(0 ~ 1)
-const get_RGBA_WithOpacity = (
-  color: string | number | chroma.Color,
-  opacity: number
-) => {
-  return chroma(color).alpha(opacity).css()
-}
-
-const change_RGBA_From_CSS = (color: string | number | chroma.Color) => {
-  return get_RGBA_Object(chroma(color).rgba())
-}
-
-// todo - split layer function for other components
-const getLayeredColor = (
-  backdrop: string,
-  source: string,
-  layerOpacity: number
-) => {
-  const backdropString = backdrop.toLowerCase().split(" ").join("")
-
-  // todo - fix white color
-  const isBackdropWhite =
-    backdropString === "white" ||
-    backdropString === "#fff" ||
-    backdropString === "#ffffff" ||
-    backdropString === "rgba(255,255,255)" ||
-    backdropString === "rgba(255,255,255,1)"
-
-  const backdropColor = isBackdropWhite
-    ? get_RGBA_WithOpacity(colorMap["gray-90"], layerOpacity * 3)
-    : get_RGBA_WithOpacity(backdrop, layerOpacity)
-
-  const layeredColor = overlay(
-    change_RGBA_From_CSS(backdropColor),
-    change_RGBA_From_CSS(source)
-  )
-
-  return get_RGBA_CSS(layeredColor)
-}
-
 export const getButtonStyleFromVariant = (variant: buttonVariants) => {
   const { hoverBackgroundColor, activeBackgroundColor } = dynamicStyles
 
@@ -89,14 +40,12 @@ export const getButtonStyleFromVariant = (variant: buttonVariants) => {
 
   return {
     [hoverBackgroundColor]: getLayeredColor(
-      labelColorCss,
       backgroundColorCss,
-      OPACITY_VALUES.HOVER
+      getBlendLayerColor(labelColorCss, "hover")
     ),
     [activeBackgroundColor]: getLayeredColor(
-      labelColorCss,
       backgroundColorCss,
-      OPACITY_VALUES.ACTIVE
+      getBlendLayerColor(labelColorCss, "pressed")
     ),
   }
 }
