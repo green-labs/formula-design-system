9f8529b7e336b126dd137c72e4be402cdfdc5317
Minsang Kim <minsang.kim@greenlabs.co.kr>
2022-11-09 22:59:44 +0900

    feat: TextDialog 스토리북 수정

--- packages/components/src/Dialog/TextDialog/TextDialog.stories.tsx
+++ packages/components/src/Dialog/TextDialog/TextDialog.stories.tsx
@@ -7,8 +7,8 @@ const TextDialogTemplate: ComponentStory<typeof TextDialog> = (args) => (
     <TextDialog {...args} />
   </div>
 )
-export const Example = TextDialogTemplate.bind({})
-Example.args = {
+export const WithTitle = TextDialogTemplate.bind({})
+WithTitle.args = {
   size: "small",
   title:
     "제목은 두줄까지입니다.\n두줄까지입니다.\n세줄은 안됩니다. 제목은 두줄까지입니다.\n두줄까지입니다.\n세줄은 안됩니다",
@@ -17,11 +17,17 @@ Example.args = {
   primaryLabel: "확인",
   secondaryLabel: "취소",
 }
-Example.parameters = {
-  layout: "fullscreen",
+
+export const WithoutTitle = TextDialogTemplate.bind({})
+WithoutTitle.args = {
+  size: "small",
+  text: "서브 텍스트의 최대 글줄은 네 줄 입니다. 그 이상이 될 경우 바텀시트를 사용해 주세요. 지금 보시는 다이얼로그가 타이틀과 서브 텍스트의 최대 글자수. 서브 텍스트의 최대 글줄은 네 줄 입니다. 그 이상이 될 경우 바텀시트를 사용해 주세요. 지금 보시는 다이얼로그가 타이틀과 서브 텍스트의 최대 글자수.",
+  buttonType: "evenly",
+  primaryLabel: "확인",
+  secondaryLabel: "취소",
 }
 
 export default {
   title: "Formula/Dialog/TextDialog",
-  component: Example,
+  component: TextDialog,
 } as ComponentMeta<typeof TextDialog>
