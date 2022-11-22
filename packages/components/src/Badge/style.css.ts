40a599d7c57ed5b2e8099194111dcf1f52465c92
Jaeho Lee <me@jaeholee.org>
2022-11-22 22:05:22 +0900

    fix(components): refactor Badge

--- packages/components/src/Badge/style.css.ts
+++ packages/components/src/Badge/style.css.ts
@@ -7,7 +7,16 @@ export const vars = {
   backgroundColor: createVar(),
 }
 
+// FIXME: need to be moved into common props
+const flexCenterContainer = style({
+  display: "inline-flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexBasis: "auto",
+})
+
 const baseBadgeStyle = style([
+  flexCenterContainer,
   {
     background: vars.backgroundColor,
     color: vars.color,
@@ -18,51 +27,34 @@ const baseBadgeStyle = style([
   },
 ])
 
-// FIXME: need to be moved into common props
-const flexCenterContainer = style({
-  display: "inline-flex",
-  justifyContent: "center",
-  alignItems: "center",
-  flexBasis: "auto",
-})
-
 export const badgeStyle = recipe({
-  base: [baseBadgeStyle],
+  base: [baseBadgeStyle, flexCenterContainer],
   variants: {
     simple: {
       small: { width: "4px", height: "4px", borderRadius: "100%" },
       medium: { width: "6px", height: "6px", borderRadius: "100%" },
     },
     contents: {
-      small: [
-        flexCenterContainer,
-        {
-          borderRadius: "8px",
-          fontSize: "12px",
-          height: "16px",
-          paddingLeft: "4px",
-          paddingRight: "4px",
-        },
-      ],
-      medium: [
-        flexCenterContainer,
-        {
-          borderRadius: "10px",
-          fontSize: "14px",
-          height: "20px",
-          paddingLeft: "6px",
-          paddingRight: "6px",
-        },
-      ],
+      small: {
+        borderRadius: "8px",
+        fontSize: "12px",
+        height: "16px",
+        paddingLeft: "4px",
+        paddingRight: "4px",
+      },
+      medium: {
+        borderRadius: "10px",
+        fontSize: "14px",
+        height: "20px",
+        paddingLeft: "6px",
+        paddingRight: "6px",
+      },
     },
     icon: {
-      small: [
-        flexCenterContainer,
-        {
-          borderRadius: "100%",
-          padding: "4px",
-        },
-      ],
+      small: {
+        borderRadius: "100%",
+        padding: "4px",
+      },
     },
   },
 })
