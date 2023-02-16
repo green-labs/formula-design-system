abc5b3530f3916ee025be11f21a27a2315b005c4
Jaeho Lee <me@jaeholee.org>
2023-02-17 03:50:40 +0900

    upgrade storybook to 7

---
+++ packages/components/.storybook/main.js
@@ -0,0 +1,24 @@
+export default {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|cjs|mjs|jsx|ts|tsx)",
+  ],
+  babel: async (options) => ({
+    // Update your babel configuration here
+    ...options,
+  }),
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react-webpack5",
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need
+    // Return the altered config
+    return config
+  },
+  core: {
+    builder: "webpack5",
+  },
+}
